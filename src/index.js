5141d55fc98b06b479eca82e9215636686973e10
chakson <373664968@qq.com>
2018-07-03 11:46:28 +0800

    add rematch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"; // Provider 就是给 connect 提供 store 用的
+import { init } from "@rematch/core";
+import * as models from './models/index';
 import './assets/style.less';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = init({
+  models,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
